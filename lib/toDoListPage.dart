a8c22f20b94cb016a278fc59c7dc3c348fe7c899
Thiago Nunes <devthiart@gmail.com>
2025-06-20 17:10:24 -0300

    Configure Firebase

--- lib/toDoListPage.dart
+++ lib/toDoListPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 class ToDoListPage extends StatefulWidget {
   // final DateTime selectedDate;
